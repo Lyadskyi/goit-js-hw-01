2b05e539f7061411d4fd7bb4026cfce2bf6fac0f
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2023-12-07 22:36:25 +0200

    Add styles.css

--- js/task-3.js
+++ js/task-3.js
@@ -7,10 +7,6 @@ function getElementWidth(content, padding, border) {
 	return totalContent + totalPadding * 2 + totalBorder * 2;
 }
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
