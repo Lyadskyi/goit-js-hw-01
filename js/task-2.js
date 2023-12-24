38dedb40023bf2e011adaae28bbb0e3409d9347d
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2023-12-24 19:31:41 +0200

    Add remarks

--- js/task-2.js
+++ js/task-2.js
@@ -6,3 +6,11 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+// ---------- REMARKS OF THE MENTOR ---------- //
+
+// task-2.js - Well executed moments/Добре виконані моменти //
+
+// Ти оголосив функцію getShippingMessage з правильними параметрами.
+// Ти використовував шаблонні літерали для створення рядка повідомлення про доставку.
+// Ти повернув отриманий рядок, як вихідні дані функції.
--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,10 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// ---------- REMARKS OF THE MENTOR ---------- //
+
+// task-3.js - Improvement/Покращення //
+
+// Не потрібно використовувати "use strict". Воно вже є стандартною поведінкою в сучасному JavaScript.
+// Виклики функцій в кінці зайві і можуть бути видалені. Функція вже тестується за допомогою операторів console.log.
